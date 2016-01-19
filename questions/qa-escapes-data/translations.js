c35edd13f1fd19c59de83cae2c2eaa9e402942b2
r12a <ishida@w3.org>
2016-01-19 20:30:41 +0000

    qa-escapes

---
+++ questions/qa-escapes-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['en', 'sv']
+
+trans.outofdatetranslations = ['de', 'es', 'pt', 'pt-br', 'ru', 'uk']
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = ['de', 'es', 'pt', 'pt-br', 'ru', 'uk']
