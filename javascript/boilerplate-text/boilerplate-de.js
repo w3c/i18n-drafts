258801c37d2e253fe340ec86e3f757437b7863d4
r12a <ishida@w3.org>
2016-01-20 19:55:54 +0000

    fr and de boilerplate

---
+++ javascript/boilerplate-text/boilerplate-de.js
@@ -0,0 +1,131 @@
+// translate the text between quotes
+// text following double slashes (such as this line) provides comments for the translator
+// words beginning with s. or f. are variables - do not disturb these ! (though you can move them)
+// where it would aid clarity, variables that occur in the text are described below
+
+
+var s = { }
+
+// DO NOT TRANSLATE
+s.currLang = {
+  'ar':'Arabisch',
+  'bg':'Bulgarisch',
+  'da':'Dänisch',
+  'de':'Deutsch',
+  'el':'Griechisch',
+  'en':'Englisch',
+  'es':'Spanisch',
+  'fr':'Französisch',
+  'he':'Hebräisch',
+  'hi':'Hindi',
+  'hu':'Ungarisch',
+  'it':'Italienisch',
+  'ja':'Japanisch',
+  'ko':'Koreanisch',
+  'nl':'Niederländisch',
+  'pl':'Polnisch',
+  'pt':'Portugiesisch',
+  'pt-br':'Brasilianisches Portugiesisch',
+  'ro':'Rumänisch',
+  'ru':'Russisch',
+  'sv':'Schwedisch',
+  'th':'Thai',
+  'tr':'Türkisch',
+  'uk':'Ukrainisch',
+  'vi':'Vietnamesisch',
+  'zh-hans':'Vereinfachtes Chinesisch',
+  'zh-hant':'Traditionelles Chinesisch',
+	}
+
+s.suppStylesheets = ''
+
+s.rtlAttribute = ""
+s.ltrAttribute = ""
+s.rlm = ""
+
+
+
+// TRANSLATE THE FOLLOWING
+s.i18nActivityHome = "Startseite der I18n-Tätigkeit"
+s.moreResourcesOfThisType = "Weitere Ressourcen diesen Typs."
+s.accessKeyN = 'Accesskey n springt zur Seitennavigation. <a href="//contentstart">Springe zum Inhalt.</a>'
+s.examplesInAnotherScript = "Dieses Dokument enthält Beispiele in einer anderen Sprache/Schrift."
+s.worldMap = "Weltkarte"
+s.searchI18nSite = "I18n-Suche" 
+s.translationDisclaimer = 'Dieses Dokument ist eine Übersetzung. Im Falle von Abweichungen oder Fehlern sollte das <a href="'+f.filename+'.en">aktuelle englische Original</a> als maßgeblich angenommen werden. Das W3C besitzt das <a href="//copyright">Copyright am Original</a>, wie unten beschrieben.'
+s.translator = "Übersetzer:"
+s.relatedLinks = "Verwandte Links"
+s.articles = "Artikel" // used in breadcrumbs, top right of page 
+s.topicIndexText = "Themenverzeichnis"
+s.techIndexText = "Technikenverzeichnis"
+s.gotoW3cHome = "Zur W3C-Startseite"
+s.gotoI18nHome = "Zur Startseite der Internationalisierungstätigkeit"
+s.internationalizationTitle = "Internationalisierung"
+s.i18nActivityHomePage = "Startseite der Internationalisierungstätigkeit."
+s.home = "Startseite"
+s.aboutI18nActivity = "Über die Internationalisierungstätigkeit."
+s.about = "Über"
+s.groupsThatMakeUp = "Gruppen, die die Internationalisierungstätigkeit bilden."
+s.groups = "Gruppen"
+s.topicIndexForInformation = "Themenindex für Informationen auf dieser Seite."
+s.topics = "Themen"
+s.taskBasedIndex = "Aufgabenorientierter Index von I18n-Techniken."
+s.techniques = "Techniken"
+s.informationResources = "Informationsquellen auf der Internationalisierungsseite."
+s.resources = "Ressourcen"
+s.newsFiltersAndFeeds = "Informationen über Nachrichten und RSS-Feeds für W3C-Internationalisierung."
+s.news = "News"
+s.onThisPage = "Auf dieser Seite"
+s.questionAlt = "Frage"
+s.questionLink = "Frage"
+s.question = "Frage"
+s.backgroundAlt = "Hintergrundinformationen"
+s.backgroundLink = "Hintergrund"
+s.background = "Hintergrund"
+s.answer = "Antwort" // heading
+s.answerAlt = "Antwort"
+s.answerLink = "Antwort"
+s.byTheWayAlt = "Weitere nützliche Informationen"
+s.byTheWayLink = "Übrigens"
+s.byTheWay = "Übrigens"
+s.furtherReadingAlt = "Literaturhinweise"
+s.furtherReadingLink = "Literaturhinweise"
+s.furtherReading = "Literaturhinweise"
+s.intendedAudience = "Zielgruppe:"
+s.skipToAnswer = "[Zur Antwort springen]"
+s.tellUsWhatYouThink = "Sagen Sie uns, was Sie denken."
+s.sendAComment = "Senden Sie uns Anmerkungen"
+s.subscribeToRSS = "Abonnieren Sie unseren RSS-Feed."
+s.newResourcesAlt = "Sie werden benachrichtigt, wenn ein neues Dokument zum ersten Mal veröffentlicht wird."
+s.newResources = "Neue Ressourcen"
+s.homePageNewsAlt = "Alle Nachrichten, die auf der Startseite gezeigt werden."
+s.homePageNews = "News auf der Startseite"
+s.followOurNews = "Follow our news feed."
+s.sentenceDelimiter = "."
+
+s.author = "Autor:" // followed by name of author(s)
+s.previousAuthors = "Vorheriger Autor:" // followed by name of previous author(s), before substantive changes were made by the current author
+s.modifiedBy = "Geändert von:" // person's name appears after colon
+s.translatedBy = "Übersetzer:"
+s.validXHTML = "Valides XHTML 1.0!"
+s.validCSS = "Valides CSS!"
+s.codedInUtf8 = "Kodiert in UTF-8!"
+
+s.translatedFromEnglishVer = "Übersetzung der englischen Version vom "+dt.enVersion+". Letzte Änderung der übersetzten Version am "+dt.thisVersionPlain+" UTC."
+
+s.historyOfDocumentChanges = 'Suchen Sie nach <span class="searchkey"><a href="http://www.w3.org/blog/International/tag/'+f.searchString+'/">'+f.searchString+'</a></span> im i18n-Blog, um alle Dokumentänderungen nachzuvollziehen.'
+s.untranslatedChanges = '<strong>Hinweis:</strong> Das  <a href="/International/'+f.directory+f.filename+'.en">englische Original</a> wurde aktualisiert. <span class="searchkey"><a href="http://www.w3.org/blog/International/tag/'+f.searchString+'/">Für Änderungen siehe Blog.</a></span>'
+s.new="Neu"// placed alongside new articles at http://www.w3.org/International/articlelist 
+s.updated="Aktualisiert" // same as New
+s.translation_updated="Übersetzung aktualisiert am" // date appears after colon 
+
+s.aboutThisArticle="Über diesen Artikel" // title in the right column near the top of the page
+s.status_draft="Dieser Artikel ist ein Entwurf und wurde noch nicht öffentlich diskutiert. Wenn Sie Anmerkungen haben, senden Sie uns diese bitte über den Link am Ende der Seite."
+s.status_review="Dieser Artikel wird zur Zeit gerade öffentlich diskutiert. Senden Sie bitte Ihre Anmerkungen an <a href='mailto:www-international@w3.org?subject=%5Breview%20feedback%5D%20"+f.filename+"'>www-international@w3.org</a><span class='noprint'> (<a href='http://lists.w3.org/Archives/Public/www-international/'>Abonnieren</a>)</span>."
+s.status_published="Dieser Artikel wurde von der Arbeits&shygruppe Inter&shynationali&shysierung des W3C geprüft und öffentlich diskutiert, damit er so korrekt wie möglich ist. Wenn Sie Anmerkungen haben, senden Sie uns diese bitte über den Link am Ende der Seite."
+s.status_notreviewed="Dieser Artikel wurde ohne vorherige öffentliche Diskussion veröffentlicht. Wenn Sie Anmerkungen haben, senden Sie uns diese bitte über den Link am Ende der Seite."
+s.tests="Tests" // used at the end of breadcrumbs for test related pages
+s.quickanswer = "Kurze Antwort" // heading
+s.longeranswer = "Ausführliche Antwort" // heading
+s.acknowledgements = "Vielen Dank für die Beiträge und Kommentare zu diesem Artikel an:" // used at bottom of page to list people who provided feedback. The list comes after this text. 
+s.cookieMsg = "If you let the browser set a cookie, you will continue to see W3C Internationalization Activity pages (where available) in the language you chose. Do you want to set the cookie?" // this text is to be copied to another location
---
+++ javascript/boilerplate-text/boilerplate-fr.js
@@ -0,0 +1,131 @@
+// translate the text between quotes
+// text following double slashes (such as this line) provides comments for the translator
+// words beginning with s. or f. are variables - do not disturb these ! (though you can move them)
+// where it would aid clarity, variables that occur in the text are described below
+
+
+var s = { }
+
+// DO NOT TRANSLATE
+s.currLang = {
+  'ar':'Arabe',
+  'bg':'Bulgare',
+  'da':'Danois',
+  'de':'Allemand',
+  'el':'Grec',
+  'en':'Anglais',
+  'es':'Espagnol',
+  'fr':'Français',
+  'he':'Hébreu',
+  'hi':'Hindi',
+  'hu':'Hongrois',
+  'it':'Italien',
+  'ja':'Japonais',
+  'ko':'Coréen',
+  'nl':'Néerlandais',
+  'pl':'Polonais',
+  'pt':'Portugais',
+  'pt-br':'Portugais brésilien',
+  'ro':'Roumain',
+  'ru':'Russe',
+  'sv':'Suédois',
+  'th':'Thaï',
+  'tr':'Turc',
+  'uk':'Ukrainien',
+  'vi':'Vietnamien',
+  'zh-hans':'Chinois simplifié',
+  'zh-hant':'Chinois traditionnel'
+	}
+
+s.suppStylesheets = ''
+
+s.rtlAttribute = ""
+s.ltrAttribute = ""
+s.rlm = ""
+
+
+
+
+// TRANSLATE THE FOLLOWING
+s.i18nActivityHome = "Accueil de l'Activité I18N";
+s.moreResourcesOfThisType = "Autres ressources du même type.";
+s.accessKeyN = 'Utiliser la touche d\'accès n pour naviguer au sein de la page. <a href="//contentstart">Sauter au début du contenu.</a>'
+s.examplesInAnotherScript = "Ce document contient des exemples dans une autre langue et une autre écriture.";
+s.worldMap = "Carte du monde";
+s.searchI18nSite = "Recherche sur le site I18N"; 
+s.translationDisclaimer = 'Ce document est une traduction. En cas de divergences ou d\'erreurs, la <a href="'+f.filename+'.en">dernière version originale en anglais</a> fait autorité. Comme indiqué ci-dessous, les <a href="//copyright">droits d\'auteur</a> reviennent au W3C.'
+s.translator = "Traducteur:";
+s.relatedLinks = "Liens utiles";
+s.articles = "Articles"; // used in breadcrumbs, top right of page 
+s.topicIndexText = "Index des sujets";
+s.techIndexText = "Index des techniques";
+s.gotoW3cHome = "Aller à la page d’accueil du W3C";
+s.gotoI18nHome = "Aller à la page d’accueil de l’activité d’internationalisation";
+s.internationalizationTitle = "Internationalisation";
+s.i18nActivityHomePage = "Page d’accueil de l’activité d’internationalisation.";
+s.home = "Accueil";
+s.aboutI18nActivity = "À propos de l’activité d’internationalisation.";
+s.about = "À propos";
+s.groupsThatMakeUp = "Les groupes constituant l’activité d’internationalisation.";
+s.groups = "Groupes";
+s.topicIndexForInformation = "Index thématique du site";
+s.topics = "Sujets";
+s.taskBasedIndex = "Index des techniques d’internationalisation par tâches";
+s.techniques = "Techniques";
+s.informationResources = "Ressources d’information sur le site de l’internationalisation.";
+s.resources = "Ressources";
+s.newsFiltersAndFeeds = "Informations sur les filtres d’information et fils RSS pour l’internationalisation du W3C.";
+s.news = "Nouvelles";
+s.onThisPage = "Sur cette page";
+s.questionAlt = "Question";
+s.questionLink = "Question";
+s.question = "Question";
+s.backgroundAlt = "Informations contextuelles";
+s.backgroundLink = "Contexte";
+s.background = "Contexte";
+s.answer = "Réponse"; // heading
+s.answerAlt = "Réponse";
+s.answerLink = "Réponse";
+s.byTheWayAlt = "Informations utiles complémentaires";
+s.byTheWayLink = "En passant";
+s.byTheWay = "En passant";
+s.furtherReadingAlt = "Pour approfondir";
+s.furtherReadingLink = "Pour approfondir";
+s.furtherReading = "Pour approfondir";
+s.intendedAudience = "Public visé :";
+s.skipToAnswer = "[Aller à la réponse]";
+s.tellUsWhatYouThink = "Donnez-nous votre avis (en anglais).";
+s.sendAComment = "Envoyez-nous un commentaire";
+s.subscribeToRSS = "Abonnez-vous au flux RSS.";
+s.newResourcesAlt = "Pour vous informer de la parution de chaque nouvelle publication.";
+s.newResources = "Nouvelles publications";
+s.homePageNewsAlt = "Chacune des nouvelles figurant en page d'accueil.";
+s.homePageNews = "Les nouvelles de la page d'accueil";
+s.followOurNews = "Follow our news feed."
+s.sentenceDelimiter = ".";
+
+s.author = "Par :"; // followed by name of author(s)
+s.previousAuthors = "Précédemment par :"; // followed by name of previous author(s), before substantive changes were made by the current author
+s.modifiedBy = "Modifié par :"; // person's name appears after colon
+s.translatedBy = "Traducteur :";
+s.validXHTML = "XHTML 1.0 valide !";
+s.validCSS = "CSS valide !";
+s.codedInUtf8 = "Codé en UTF-8 !";
+
+s.translatedFromEnglishVer = "Traduit d’un contenu en anglais daté du "+dt.enVersion+". Dernière modification de cette traduction le "+dt.thisVersionPlain+" GMT."; 
+
+s.historyOfDocumentChanges = 'Pour un résumé des changements importants, recherchez <span class="searchkey"><a href="http://www.w3.org/blog/International/tag/'+f.searchString+'/">'+f.searchString+'</a></span> dans le blog i18n.'
+s.untranslatedChanges = '<strong>Note:</strong>  Des changements ont été apportés au <a href="/International/'+f.directory+f.filename+'.en">document original en anglais</a> depuis sa traduction. <span class="searchkey"><a href="http://www.w3.org/blog/International/tag/'+f.searchString+'/">Consulter le fichier des changements.</a></span>'
+s.new="New";// placed alongside new articles at http://www.w3.org/International/articlelist 
+s.updated="Updated"; // same as New
+s.translation_updated="Translation updated:"; // date appears after colon 
+
+s.aboutThisArticle="À propos de cet article"; // title in the right column near the top of the page
+s.status_draft="Cet article est un brouillon qui n’a pas encore été proposé à relecture publique. Si certains points vous semblent à débattre, merci d’envoyer vos remarques en utilisant le lien de bas de page";
+s.status_review="Cet article est proposé à relecture publique. Merci d'envoyer vos commentaires à <a href='mailto:www-international@w3.org?subject=%5Breview%20feedback%5D%20"+f.filename+"'>www-international@w3.org</a><span class='noprint'> (<a href='http://lists.w3.org/Archives/Public/www-international/'>s’abonner</a>)</span>.";
+s.status_published="Cet article a été relu par le Groupe de Travail Internationalisation du W3C et a subi un processus de relecture publique pour le rendre aussi précis que possible. Si certains points vous semblent à débattre, merci d’envoyer vos remarques en utilisant le lien de bas de page.";
+s.status_notreviewed="Cet article n’a pas fait l’objet d’une relecture publique. Si certains points vous semblent à débattre, merci d’envoyer vos remarques en utilisant le lien de bas de page.";
+s.tests=""; // used at the end of breadcrumbs for test related pages
+s.quickanswer = "Réponse courte"; // heading
+s.longeranswer = "Réponse longue"; // heading
+s.cookieMsg = "Si vous laissez le navigateur définir un cookie, vous continuerez à voir les pages de l'Activité Internationalisation du W3C (si disponible) dans la langue que vous avez choisi. Voulez-vous définir le cookie?" // this text is to be copied to another location
