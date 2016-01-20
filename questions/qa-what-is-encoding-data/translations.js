d755f82e19a8d4fed6f86abbd470d8ccf0158425
r12a <ishida@w3.org>
2016-01-20 19:57:41 +0000

    qa-what-is-encoding added

---
+++ questions/qa-what-is-encoding-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de', 'en', 'fr', 'sv']
+
+trans.outofdatetranslations = ['es', 'hu', 'pl', 'pt-br', 'ro', 'ru', 'uk']
+
+trans.updatedtranslations = ['de', 'sv']
+
+trans.unlinkedtranslations = ['es', 'hu', 'pl', 'pt-br', 'ro', 'ru', 'uk']
