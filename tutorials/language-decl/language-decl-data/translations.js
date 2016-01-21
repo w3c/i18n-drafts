2ee5534406df93712c889e5d6c452a9ce20f21e4
r12a <ishida@w3.org>
2016-01-21 14:51:08 +0000

    language-decl tutorial added

---
+++ tutorials/language-decl/language-decl-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de', 'en', 'es', 'fr', 'uk']
+
+trans.outofdatetranslations =['hu']
+
+trans.updatedtranslations = ['es', 'ru', 'uk']
+
+trans.unlinkedtranslations = ['hu', 'ru']
