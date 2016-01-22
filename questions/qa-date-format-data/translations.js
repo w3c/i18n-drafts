de510727493985743292fded281a8c36c62a5249
r12a <ishida@w3.org>
2016-01-22 15:37:08 +0000

    qa-date-format baseline for all languages

--- questions/qa-date-format-data/translations.js
+++ questions/qa-date-format-data/translations.js
@@ -1,8 +1,9 @@
 var trans = { }
 
-trans.versions = ['de', 'en', 'es', 'hu', 'ro', 'ru', 'sv', 'uk']
+trans.versions = ['de', 'en', 'es', 'hu', 'ro', 'sv', 'uk']
 
 trans.outofdatetranslations = []
 
 trans.updatedtranslations = []
 
+trans.unlinkedtranslations = ['ru']
