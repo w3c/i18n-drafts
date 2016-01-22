8133022476d27851c6aa5938e7628c3022e29562
r12a <ishida@w3.org>
2016-01-22 15:34:52 +0000

    hu and ru boilerplate

---
+++ javascript/boilerplate-text/boilerplate-hu.js
@@ -0,0 +1,130 @@
+// translate the text between quotes
+// text following double slashes (such as this line) provides comments for the translator
+// words beginning with s. or f. are variables - do not disturb these ! (though you can move them)
+// where it would aid clarity, variables that occur in the text are described below
+
+var s = { }
+
+// DO NOT TRANSLATE
+s.currLang = {
+  'ar':'Arab',
+  'bg':'Bolgár',
+  'da':'Dán',
+  'de':'Német',
+  'el':'Görög',
+  'en':'Angol',
+  'es':'Spanyol',
+  'fr':'Francia',
+  'he':'Héber',
+  'hi':'Hindi',
+  'hu':'Magyar',
+  'it':'Olasz',
+  'ja':'Japán',
+  'ko':'Koreai',
+  'nl':'Holland',
+  'pl':'Lengyel',
+  'pt':'Portugál',
+  'pt-br':'Brazíliai Portugál',
+  'ro':'Román',
+  'ru':'Orosz',
+  'sv':'Svéd',
+  'th':'Thai',
+  'tr':'Török',
+  'uk':'Ukrán',
+  'vi':'Vietnámi',
+  'zh-hans':'Egyszerűsített kínai',
+  'zh-hant':'Hagyományos kínai'
+}
+
+s.suppStylesheets = ''
+
+s.rtlAttribute = ""
+s.ltrAttribute = ""
+s.rlm = ""
+
+
+
+// TRANSLATE THE FOLLOWING
+s.i18nActivityHome = "I18N Tevékenység Kezdőoldala"
+s.moreResourcesOfThisType = "Több ehhez hasonló forrás."
+s.accessKeyN = 'Az n billentyű átugrik az <a href="//internal-links" accesskey="n">oldal navigációhoz</a>. <a href="//contentstart">Ugrás a szöveg elejére.</a>'
+s.examplesInAnotherScript = "Ez a dokumentum más nyelvű példákat tartalmaz."
+s.worldMap = "Világtérkép"
+s.searchI18nSite = "Keresés az I18n oldalán" 
+s.translationDisclaimer = 'Ez a dokumentum egy fordítás. Bármilyen ellentmondás vagy hiba esetén a <a href="'+f.filename+'.en">legfrissebb angol nyelvű eredeti változatot</a> kell mérvadónak tekinteni. <a href="//copyright">A szerzői jog</a> a W3C tulajdonát képezi, amint az alább látható.'
+s.translator = "Fordító:"
+s.relatedLinks = "Kapcsolódó linkek"
+s.articles = "Articles" // used in breadcrumbs, top right of page 
+s.topicIndexText = "Témák"
+s.techIndexText = "Módszerek"
+s.gotoW3cHome = "Tovább a W3C Kezdőoldalára"
+s.gotoI18nHome = "Tovább az Internacionalizációs Tevékenység Kezdőoldalára"
+s.internationalizationTitle = "Internacionalizációs Tevékenység"
+s.i18nActivityHomePage = "Internacionalizációs Tevékenység Kezdőoldala."
+s.home = "Kezdőoldal"
+s.aboutI18nActivity = "Az Internacionalizációs Tevékenységünkről."
+s.about = "Magunkról"
+s.groupsThatMakeUp = "Csoportok akik az Internacionalizációs Tevékenységgel foglalkoznak."
+s.groups = "Csoportok"
+s.topicIndexForInformation = "Témák névmutatója az oldalon található információkhoz."
+s.topics = "Témák"
+s.taskBasedIndex = "Feladat alapú névmutató az i18n módszerekhez."
+s.techniques = "Módszerek"
+s.informationResources = "Információs források az oldalon."
+s.resources = "Források"
+s.newsFiltersAndFeeds = "Információk a hírszűrőkről és RSS feed-ekről"
+s.news = "Hírek"
+s.onThisPage = "Ezen az oldalon"
+s.questionAlt = "Kérdés"
+s.questionLink = "Kérdés"
+s.question = "Kérdés"
+s.backgroundAlt = "Háttérinformáció"
+s.backgroundLink = "Háttér"
+s.background = "Háttér"
+s.answer = "Válasz" // heading
+s.answerAlt = "Válasz"
+s.answerLink = "Válasz"
+s.byTheWayAlt = "Hasznos kiegészítő információ"
+s.byTheWayLink = "Mellesleg"
+s.byTheWay = "Mellesleg"
+s.furtherReadingAlt = "További olvasnivaló"
+s.furtherReadingLink = "További olvasnivaló"
+s.furtherReading = "További olvasnivaló"
+s.intendedAudience = "Célközönség:"
+s.skipToAnswer = "[Ugrás a válaszhoz]"
+s.tellUsWhatYouThink = "Mondja el nekünk mit gondol!"
+s.sendAComment = "Küldjön kommentet"
+s.subscribeToRSS = "Feliratkozás RSS Feed-re."
+s.newResourcesAlt = "Értesíti Önt amikor első alkalommal lett új forrás publikálva."
+s.newResources = "Új források"
+s.homePageNewsAlt = "Minden hír a kezdőoldalon."
+s.homePageNews = "Kezdőoldal hírek"
+s.followOurNews = "Follow our news feed."
+s.sentenceDelimiter = "."
+
+s.author = "Szerző:" // followed by name of author(s)
+s.previousAuthors = "" // followed by name of previous author(s), before substantive changes were made by the current author
+s.modifiedBy = "Módosítás:" // person's name appears after colon
+s.translatedBy = "Fordító:"
+s.validXHTML = "Valid XHTML 1.0!"
+s.validCSS = "Valid CSS!"
+s.codedInUtf8 = "UTF-8-ben kódolva!"
+
+s.translatedFromEnglishVer = "Angolról fordítva: "+dt.enVersion+". A lefordított verzió utolsó módosítása: "+dt.thisVersionPlain+" GMT"
+
+s.historyOfDocumentChanges = 'A dokumentum módosításainak a történetéhez keresse ezt az i18n blogban: <span class="searchkey"><a href="http://www.w3.org/blog/International/tag/'+f.searchString+'/">'+f.searchString+'</a></span>'
+s.untranslatedChanges = '<strong>Megjegyzés:</strong> Változások történtek <a href="'+f.filename+'.en.php">az eredeti angol változatban</a> amióta a dokumentum le lett fordítva. <span class="searchkey"><a href="http://www.w3.org/blog/International/tag/'+f.searchString+'/">Változások megtekintése.</a></span>'
+s.new="New"// placed alongside new articles at http://www.w3.org/International/articlelist 
+s.updated="Updated" // same as New
+s.translation_updated="Translation updated:" // date appears after colon 
+
+s.aboutThisArticle="About this article" // title in the right column near the top of the page
+s.status_draft="This article is a draft that has not yet gone through public review. If there are things that need addressing, please send us feedback using the link near the bottom of the page."
+s.status_review="This article is currently out for public review. Please send comments to <a href='mailto:www-international@w3.org?subject=%5Breview%20feedback%5D%20"+f.directory+f.filename+"'>www-international@w3.org</a><span class='noprint'> (<a href='http://lists.w3.org/Archives/Public/www-international/'>subscribe</a>)</span>."
+s.status_published="This article has been reviewed by the W3C Internationalization Working Group and has gone through public review to make it as accurate as possible. If there are things that need addressing, please send us feedback using the link near the bottom of the page."
+s.status_notreviewed="This article was published without public review. If there are things that need addressing, please send us feedback using the link near the bottom of the page."
+s.tests="Tests" // used at the end of breadcrumbs for test related pages
+s.quickanswer = "Quick answer" // heading
+s.longeranswer = "Longer answer" // heading
+s.acknowledgements = "Thanks also to the following people whose contribution or feedback was included:" // used at bottom of page to list people who provided feedback. The list comes after this text. 
+s.cookieMsg = "If you let the browser set a cookie, you will continue to see W3C Internationalization Activity pages (where available) in the language you chose. Do you want to set the cookie?" // this text is to be copied to another location
---
+++ javascript/boilerplate-text/boilerplate-ro.js
@@ -0,0 +1,131 @@
+// translate the text between quotes
+// text following double slashes (such as this line) provides comments for the translator
+// words beginning with s. or f. are variables - do not disturb these ! (though you can move them)
+// where it would aid clarity, variables that occur in the text are described below
+
+var s = { }
+
+// DO NOT TRANSLATE
+s.currLang = {
+  'ar':'Arabă',
+  'bg':'Bulgară',
+  'da':'Daneză',
+  'de':'Germană',
+  'el':'Greacă',
+  'en':'Engleză',
+  'es':'Spaniolă',
+  'fr':'Franceză',
+  'he':'Ebraică',
+  'hi':'Hindi',
+  'hu':'Maghiară',
+  'it':'Italiană',
+  'ja':'Japoneză',
+  'ko':'Coreeană',
+  'nl':'Olandeză',
+  'pl':'Poloneză',
+  'pt':'Portugheză',
+  'pt-br':'Portugheză',
+  'ro':'Română',
+  'ru':'Rusă',
+  'sv':'Suedeză',
+  'th':'Thailandeză',
+  'tr':'Turcă',
+  'uk':'Ucraineană',
+  'vi':'Vietnameză',
+  'zh-hans':'Chineză simplificată',
+  'zh-hant':'Chineză tradițională',
+}
+
+s.suppStylesheets = ''
+
+s.rtlAttribute = ""
+s.ltrAttribute = ""
+s.rlm = ""
+
+
+
+
+// TRANSLATE THE FOLLOWING
+s.i18nActivityHome = "Pagina de start a activitatii I18N"
+s.moreResourcesOfThisType = "Mai multe resurse de acest tip"
+s.accessKeyN = 'Cheia de acces n sare la<a href="//internal-links" accesskey="n">nagigarea in pagina</a>. <a href="//contentstart">Sari la inceputul continutului.</a>'
+s.examplesInAnotherScript = "Acest document contine exemple in alta limba/script."
+s.worldMap = "Harta lumii"
+s.searchI18nSite = "Cauta in site-ul I18n" 
+s.translationDisclaimer = 'Acest document e o traducere. In caz ca apare vreo eroare sau discrepanta, <a href="'+f.filename+'.en">ultima versiune in Engleza</a> este cea autoritativa. <a href="//copyright">Copyright-ul original</a> apartine W3C, dupa cum e aratat mai jos.'
+s.translator = "Traducator:"
+s.relatedLinks = "Alte linkuri"
+s.articles = "Articole" // used in breadcrumbs, top right of page 
+s.topicIndexText = "Indexul subiectelor"
+s.techIndexText = "Indexul tehnicilor"
+s.gotoW3cHome = "Pagina principala a W3C"
+s.gotoI18nHome = "Pagina principala a activitatii de Internationalizare"
+s.internationalizationTitle = "Internationalizare"
+s.i18nActivityHomePage = "Pagina principala a Activitatii de Internationalizare."
+s.home = "Acasa"
+s.aboutI18nActivity = "Despre Activitatea de Internationalizare."
+s.about = "Despre"
+s.groupsThatMakeUp = "Grupuri care constituie Activitatea de Internationalizare."
+s.groups = "Grupuri"
+s.topicIndexForInformation = "Cuprinsul informatiilor din acest site."
+s.topics = "Subiecte"
+s.taskBasedIndex = "Cuprinsul tehnicilor de internationalizare."
+s.techniques = "Tehnici"
+s.informationResources = "Alte informatii pe site-ul Internationalizarii."
+s.resources = "Resurse"
+s.newsFiltersAndFeeds = "Informatii despre filtre noi si RSS feeds pentru Internationalizarea W3C."
+s.news = "Noutati"
+s.onThisPage = "Pe aceasta pagina"
+s.questionAlt = "Intrebare"
+s.questionLink = "Intrebare"
+s.question = "Intrebare"
+s.backgroundAlt = "Informatii introductive"
+s.backgroundLink = "Introducere"
+s.background = "Introducere"
+s.answer = "Raspuns" // heading
+s.answerAlt = "Raspuns"
+s.answerLink = "Raspuns"
+s.byTheWayAlt = "Alte informatii utile"
+s.byTheWayLink = "Apropo"
+s.byTheWay = "Apropo"
+s.furtherReadingAlt = "Alte materiale"
+s.furtherReadingLink = "Alte materiale"
+s.furtherReading = "Alte materiale"
+s.intendedAudience = "Cititorii vizati:"
+s.skipToAnswer = "[Sari direct la raspuns]"
+s.tellUsWhatYouThink = "Spune-ne părerea ta."
+s.sendAComment = "Trimite un comentariu"
+s.subscribeToRSS = "Abonează-te la RSS feed."
+s.newResourcesAlt = "Te informează de fiecare dată când noi resurse sunt publicate pentru prima dată."
+s.newResources = "Resurse noi"
+s.homePageNewsAlt = "Toate noutăţile prezentate pe prima pagina."
+s.homePageNews = "Noutăţi prima pagină"
+s.followOurNews = "Follow our news feed."
+s.sentenceDelimiter = "."
+
+s.author = "Autor:" // followed by name of author(s)
+s.previousAuthors = "" // followed by name of previous author(s), before substantive changes were made by the current author
+s.modifiedBy = "Modificat de:" // person's name appears after colon
+s.translatedBy = "Traducator:"
+s.validXHTML = "XHTML 1.0 Valid!"
+s.validCSS = "CSS Valid!"
+s.codedInUtf8 = "Incodat cu UTF-8!"
+
+s.translatedFromEnglishVer = "Tradus din engleza: "+dt.enVersion+". Ultima modificare a traducerii: "+dt.thisVersionPlain+" GMT"
+
+s.historyOfDocumentChanges = 'Pentru a vedea toate schimbarile documentului, cauta <span class="searchkey"><a href="http://www.w3.org/blog/International/tag/'+f.searchString+'/">'+f.searchString+'</a></span> pe blogul i18n.'
+s.untranslatedChanges = '<strong>Nota:</strong> Au fost efectuate modificari asupra  <a href="'+f.filename+'.en">versiunii originale in Engleza</a> dupa ce a fost tradus. <span class="searchkey"><a href="http://www.w3.org/blog/International/tag/'+f.searchString+'/">Vezi istoricul schimbarii.</a></span>'
+s.new="Nou"// placed alongside new articles at http://www.w3.org/International/articlelist 
+s.updated="Actualizat" // same as New
+s.translation_updated="Translation updated:" // date appears after colon 
+
+s.aboutThisArticle="About this article" // title in the right column near the top of the page
+s.status_draft="This article is a draft that has not yet gone through public review. If there are things that need addressing, please send us feedback using the link near the bottom of the page."
+s.status_review="This article is currently out for public review. Please send comments to <a href='mailto:www-international@w3.org?subject=%5Breview%20feedback%5D%20"+f.directory+f.filename+"'>www-international@w3.org</a><span class='noprint'> (<a href='http://lists.w3.org/Archives/Public/www-international/'>subscribe</a>)</span>."
+s.status_published="This article has been reviewed by the W3C Internationalization Working Group and has gone through public review to make it as accurate as possible. If there are things that need addressing, please send us feedback using the link near the bottom of the page."
+s.status_notreviewed="This article was published without public review. If there are things that need addressing, please send us feedback using the link near the bottom of the page."
+s.tests="Tests" // used at the end of breadcrumbs for test related pages
+s.quickanswer = "Quick answer" // heading
+s.longeranswer = "Longer answer" // heading
+s.acknowledgements = "Thanks also to the following people whose contribution or feedback was included:" // used at bottom of page to list people who provided feedback. The list comes after this text. 
+s.cookieMsg = "If you let the browser set a cookie, you will continue to see W3C Internationalization Activity pages (where available) in the language you chose. Do you want to set the cookie?" // this text is to be copied to another location
