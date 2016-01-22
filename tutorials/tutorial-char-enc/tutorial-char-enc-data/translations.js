6c4b52bc091dc3b31d60ff35cdd8106e3f44226f
r12a <ishida@w3.org>
2016-01-22 19:48:27 +0000

    tutorial-char-enc added with augmented translation baseline (example in nutshell)

---
+++ tutorials/tutorial-char-enc/tutorial-char-enc-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de', 'en', 'ro', 'sv']
+
+trans.outofdatetranslations = ['da', 'pl', 'ru', 'uk']
+
+trans.updatedtranslations = ['de','ro'];
+
+trans.unlinkedtranslations = ['da', 'pl', 'ru', 'uk']
