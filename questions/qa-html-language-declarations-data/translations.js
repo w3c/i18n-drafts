e9bb6d66b655abb9e8dd84d2f4e152937e81d9bb
r12a <ishida@w3.org>
2016-01-25 08:42:26 +0000

    qa-html-language-declarations added

---
+++ questions/qa-html-language-declarations-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de', 'en']
+
+trans.outofdatetranslations = ['es', 'ru', 'uk']
+
+trans.updatedtranslations = ['de']
+
+trans.unlinkedtranslations = ['es', 'ru', 'uk']
