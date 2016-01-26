a0fecbf557961b2c1fc88a29da383a35cb04a16a
r12a <ishida@w3.org>
2016-01-26 09:47:43 +0000

    qa-choosing-encodings added

---
+++ questions/qa-choosing-encodings-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de', 'en', 'pt-br', 'sv'];
+
+trans.outofdatetranslations = ['es', 'ru', 'uk'];
+
+trans.updatedtranslations = ['de', 'pt-br', 'sv'];
+
+trans.unlinkedtranslations = ['es', 'ru', 'uk'];
