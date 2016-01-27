20cb7ce8192176637732bed79ede5f7a5c40469f
r12a <ishida@w3.org>
2016-01-27 10:55:43 +0000

    qa-display-capabilities added

---
+++ questions/qa-display-capabilities-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de', 'en', 'es', 'fr', 'hu', 'ro', 'sv', 'uk'];
+
+trans.outofdatetranslations = [];
+
+trans.updatedtranslations = [];
+
+trans.unlinkedtranslations = ['ru'];
