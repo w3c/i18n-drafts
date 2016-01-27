c6bb54fa243b7823aca469184bc183bfd4e9ba0f
r12a <ishida@w3.org>
2016-01-27 09:16:36 +0000

    qa-html-encoding-declarations added

---
+++ questions/qa-html-encoding-declarations-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de', 'en', 'sv'];
+
+trans.outofdatetranslations = [];
+
+trans.updatedtranslations = ['de', 'sv'];
+
+trans.unlinkedtranslations = ['es', 'ru', 'uk'];
