ba0b1055007340c4e621539c6ace58f88389ae65
r12a <ishida@w3.org>
2016-01-30 06:20:07 +0000

    qa-forms-utf-8 added

---
+++ questions/qa-forms-utf-8-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['ar','de','el','en','es','fr','hu','ja','ko','pl','ru','uk','zh-hans']
+
+trans.outofdatetranslations = []
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = []
