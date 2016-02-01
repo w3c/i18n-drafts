8b2cf56482f5ec22a4762aeb15c3aff6bd994b28
r12a <ishida@w3.org>
2016-02-01 15:56:22 +0000

    article-text-size added

---
+++ articles/article-text-size-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['ar','de','en','es','hu','ja','ko','pl','ro','ru','uk']
+
+trans.outofdatetranslations = []
+
+trans.updatedtranslations = [];
+
+trans.unlinkedtranslations = []
