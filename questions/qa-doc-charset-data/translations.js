19bba7b03701301f2f5f9031b3447cb4c303559b
r12a <ishida@w3.org>
2016-02-07 16:26:02 +0000

    qa-doc-charset added

---
+++ questions/qa-doc-charset-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de','en','es','hi','hu','it','ja','pl','pt','pt-br','ro','ru','sv','uk']
+
+trans.outofdatetranslations = []
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = []
