f2a699d0f2cc3cf2d467b2d6690fdfbcabf9ca91
r12a <ishida@w3.org>
2016-02-08 20:34:42 +0000

    boilerplate es, added Details string

--- javascript/boilerplate-text/boilerplate-es.js
+++ javascript/boilerplate-text/boilerplate-es.js
@@ -128,6 +128,7 @@ s.status_obsolete="This article is now obsolete. It is no longer maintained and 
 s.tests="" // used at the end of breadcrumbs for test related pages
 s.quickanswer = "Respuesta rápida" // heading
 s.longeranswer = "Respuesta extensa" // heading
+s.details = "Detalles" // heading
 s.additionalinfo = "Additional information" // heading that sometimes follows 'Details'
 s.acknowledgements = "Agradecemos también a las siguientes personas cuyas contribuciones y opiniones fueron incluidas:" // used at bottom of page to list people who provided feedback. The list comes after this text. 
 s.cookieMsg = "Si permite que el navegador establezca una cookie, continuará viendo las páginas de Actividad de Internacionalización de W3C (si existieran) en el idioma de su elección. ¿Quiere establecer la cookie?" // this text is to be copied to another location
