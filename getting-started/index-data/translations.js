75814554835c34c3ff67723532b8747ac5ea8fcb
r12a <ishida@w3.org>
2016-02-08 12:55:23 +0000

    getting-started index added

---
+++ getting-started/index-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['ar', 'de', 'en', 'es', 'fr', 'pt', 'pt-br', 'ro', 'uk']
+
+trans.outofdatetranslations = []
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = ['ru']
