5cf4f91c1f27f9bda7424eb06822eb0270b38aa6
r12a <ishida@w3.org>
2016-02-20 11:30:33 +0400

    qa-mono-multilingual added

---
+++ questions/qa-mono-multilingual-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['ar','de','en','es','hu','pl','ro','ru','sv','uk']
+
+trans.outofdatetranslations = []
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = []
