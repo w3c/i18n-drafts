96450da3c674f471f5c2e3cd9ac7f3331d163bdc
r12a <ishida@w3.org>
2016-02-21 13:25:57 +0400

    serving-xhtml added

---
+++ articles/serving-xhtml/index-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de','en','es','pt-br','ru','sv','uk'];
+
+trans.outofdatetranslations = [];
+
+trans.updatedtranslations = ['pt-br','sv'];
+
+trans.unlinkedtranslations = ['fr','pl','ro','th'];
