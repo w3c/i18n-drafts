b801c4191dedaecdba04c4f3db5b9871deb0cdb5
r12a <ishida@w3.org>
2016-03-01 20:48:51 +0000

    qa-headers-charset added

---
+++ questions/qa-headers-charset-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de','en','es','fr','hu','ko','pl','pt-br','ro','sv']
+
+trans.outofdatetranslations = ['fr','hu','pl']
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = ['ru','uk']
