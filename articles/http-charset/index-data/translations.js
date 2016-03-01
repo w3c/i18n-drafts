3d0cfbc6ebba9a737b1432b9f0bcd646943ca437
r12a <ishida@w3.org>
2016-03-01 09:43:25 +0000

    articles/http-charset added

---
+++ articles/http-charset/index-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de','en','es','fr','hu','pl','pt-br','ro','ru','sv','uk']
+
+trans.outofdatetranslations = []
+
+trans.updatedtranslations = [];
+
+trans.unlinkedtranslations = []
