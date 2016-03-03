a2c6b8aed58603d8b26abbaa33765840193273d1
r12a <ishida@w3.org>
2016-03-03 16:18:22 +0000

    qa-controls added

---
+++ questions/qa-controls-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de','en','es','pl','pt-br','ro','ru','uk']
+
+trans.outofdatetranslations = []
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = []
