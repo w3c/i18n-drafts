475d66c0586bb321ab85fb0c391829b9c15aef72
r12a <ishida@w3.org>
2016-03-08 10:13:49 +0000

    qa-when-lang-neg added

---
+++ questions/qa-when-lang-neg-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de','en','fr','hu','pl','ro','ru','uk']
+
+trans.outofdatetranslations = []
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = []
