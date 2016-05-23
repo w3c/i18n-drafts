627f4669f14a142b5c9ebacf056d282767e43ad5
r12a <ishida@w3.org>
2016-05-23 15:31:54 +0100

    added

---
+++ questions/qa-changing-encoding-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['en']
+
+trans.outofdatetranslations = []
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = ['bg','de','es','fr','hi','hu','pl','pt','pt-br','ro','ru','sv','uk','zh-hans']
