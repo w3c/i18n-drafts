d8ccf59bdc0ea08d3a252eaea116d91ef3a6f59e
r12a <ishida@w3.org>
2016-05-25 11:42:07 +0100

    qa-lang-why added

---
+++ questions/qa-lang-why-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['de', 'en','es','fr','ru','sv','uk','zh-hans']
+
+trans.outofdatetranslations = []
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = ['hu','pl']
