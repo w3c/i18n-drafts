2b6feafc0d705256837be67793662a36c520f82a
r12a <ishida@w3.org>
2016-05-25 11:41:35 +0100

    qa-lang-2or3 added

---
+++ questions/qa-lang-2or3-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['bg','de','el','en','es','fr','pl','ro','ru','uk']
+
+trans.outofdatetranslations = ['el']
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = []
