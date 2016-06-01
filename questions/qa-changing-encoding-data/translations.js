04d9cf73de2b4339cc4ff32f0ecbb0b005ce6900
r12a <ishida@w3.org>
2016-06-01 20:53:43 +0100

    qa-changing-encoding, fr translation

--- questions/qa-changing-encoding-data/translations.js
+++ questions/qa-changing-encoding-data/translations.js
@@ -1,9 +1,9 @@
 var trans = { }
 
-trans.versions = ['en']
+trans.versions = ['en','fr']
 
 trans.outofdatetranslations = []
 
-trans.updatedtranslations = []
+trans.updatedtranslations = ['fr']
 
-trans.unlinkedtranslations = ['bg','de','es','fr','hi','hu','pl','pt','pt-br','ro','ru','sv','uk','zh-hans']
+trans.unlinkedtranslations = ['bg','de','es','hi','hu','pl','pt','pt-br','ro','ru','sv','uk','zh-hans']
