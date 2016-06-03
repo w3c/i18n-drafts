e94ef92ce86ad68c32cc7a194a5fd7e7469974e9
r12a <ishida@w3.org>
2016-06-03 11:32:54 +0100

    qa-navigation-select added

---
+++ questions/qa-navigation-select-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['ar','de','en','es','pl','pt-br','ru','uk','zh-hans','zh-hant']
+
+trans.outofdatetranslations = []
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = []
