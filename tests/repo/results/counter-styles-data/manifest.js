6fd6656ff74a684f56ed1347c3e18a22cd192f81
r12a <ishida@w3.org>
2018-03-01 09:44:47 +0000

    + results/counter-styles

---
+++ tests/repo/results/counter-styles-data/manifest.js
@@ -0,0 +1,112 @@
+var tests = {
+	'counter-style-system/additive-basic-001.html': [
+	'additive basic, simple',
+	'https://drafts.csswg.org/css-counter-styles/#additive-system',
+	'The browser can produce an additive custom counter style.',
+	],
+	'counter-style-system/additive-basic-002.html': [
+	'additive basic, extended',
+	'https://drafts.csswg.org/css-counter-styles/#additive-system',
+	'The browser can produce an additive custom counter style.',
+	],
+	'counter-style-system/alphabetic-basic-001.html': [
+	'alphabetic basic, simple',
+	'https://drafts.csswg.org/css-counter-styles/#alphabetic-system',
+	'The browser can produce an alphabetic custom counter style.',
+	],
+	'counter-style-system/alphabetic-basic-002.html': [
+	'alphabetic basic, extended',
+	'https://drafts.csswg.org/css-counter-styles/#alphabetic-system',
+	'The browser can produce an alphabetic custom counter style.',
+	],
+	'counter-style-system/cyclic-basic-001.html': [
+	'cyclic basic, 1 symbol',
+	'',
+	'The browser can produce a cyclic custom counter style.',
+	],
+	'counter-style-system/cyclic-basic-002.html': [
+	'cyclic basic, 4 symbols',
+	'',
+	'The browser can produce a cyclic custom counter style.',
+	],
+	'counter-style-system/fixed-basic-001.html': [
+	'fixed basic',
+	'http://www.w3.org/TR/css-counter-styles-3/#simple-numeric',
+	'The browser can produce a fixed custom counter style.',
+	],
+	'counter-style-system/numeric-basic-001.html': [
+	'numeric basic, simple',
+	'http://www.w3.org/TR/css-counter-styles-3/#simple-numeric',
+	'The browser can produce a numeric custom counter style.',
+	],
+	'counter-style-system/numeric-basic-002.html': [
+	'numeric basic, extended',
+	'http://www.w3.org/TR/css-counter-styles-3/#simple-numeric',
+	'The browser can produce a numeric custom counter style.',
+	],
+	'counter-style-system/symbolic-basic-001.html': [
+	'symbolic basic',
+	'http://www.w3.org/TR/css-counter-styles-3/#simple-numeric',
+	'The browser can produce a symbolic custom counter style.',
+	],
+	'counter-style-affix/additive-prefsuf-001.html': [
+	'prefix/suffix (additive)',
+	'',
+	'The browser can produce a prefix and suffix as specified for additive counters.',
+	],
+	'counter-style-affix/alphabetic-prefsuf-001.html': [
+	'prefix/suffix (alphabetic)',
+	'',
+	'The browser can produce a prefix and suffix as specified for alphabetic counters.',
+	],
+	'counter-style-affix/cyclic-prefsuf-001.html': [
+	'prefix/suffix (cyclic)',
+	'',
+	'The browser can produce a prefix and suffix as specified for cyclic counters.',
+	],
+	'counter-style-affix/fixed-prefsuf-001.html': [
+	'prefix/suffix (fixed)',
+	'',
+	'The browser can produce a prefix and suffix as specified for fixed counters.',
+	],
+	'counter-style-affix/numeric-prefsuf-001.html': [
+	'prefix/suffix (numeric)',
+	'',
+	'The browser can produce a prefix and suffix as specified for numeric counters.',
+	],
+	'counter-style-affix/symbolic-prefsuf-001.html': [
+	'prefix/suffix (symbolic)',
+	'',
+	'The browser can produce a prefix and suffix as specified for symbolic counters.',
+	],
+	'counter-style-affix/additive-default-001.html': [
+	'prefix/suffix default (additive)',
+	'',
+	'The browser can produce a default prefix and suffix as specified for additive counters.',
+	],
+	'counter-style-affix/alphabetic-default-001.html': [
+	'prefix/suffix default (alphabetic)',
+	'',
+	'The browser can produce a default prefix and suffix as specified for alphabetic counters.',
+	],
+	'counter-style-affix/cyclic-default-001.html': [
+	'prefix/suffix default (cyclic)',
+	'',
+	'The browser can produce a default prefix and suffix as specified for cyclic counters.',
+	],
+	'counter-style-affix/fixed-default-001.html': [
+	'prefix/suffix default (fixed)',
+	'',
+	'The browser can produce a default prefix and suffix as specified for fixed counters.',
+	],
+	'counter-style-affix/numeric-default-001.html': [
+	'prefix/suffix default (numeric)',
+	'',
+	'The browser can produce a default prefix and suffix as specified for numeric counters.',
+	],
+	'counter-style-affix/symbolic-default-001.html': [
+	'prefix/suffix default (symbolic)',
+	'',
+	'The browser can produce a default prefix and suffix as specified for symbolic counters.',
+	],
+}
