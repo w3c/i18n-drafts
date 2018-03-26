6e351e22bc5b613b9740a5336feade2bd76ac940
r12a <ishida@w3.org>
2018-03-26 17:11:38 +0100

    gs language: template update

---
+++ getting-started/language-data/translations.js
@@ -0,0 +1,10 @@
+var trans = { }
+
+trans.versions = ['ar', 'de', 'en', 'fr', 'hi', 'hu', 'ja', 'ko', 'pl', 'pt-br', 'ro', 'ru', 'sv', 'uk', 'zh-hans']
+
+trans.outofdatetranslations = ['es', 'pt']
+
+trans.updatedtranslations = []
+
+trans.unlinkedtranslations = ['es', 'pt']
+
