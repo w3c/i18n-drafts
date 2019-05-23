0e75bcd1cb3c2f06038b9959f5dba373e06956bd
r12a <ishida@w3.org>
2019-05-23 16:16:24 +0100

    + qa-utf8-bom

---
+++ questions/qa-utf8-bom-data/translations.js
@@ -0,0 +1,9 @@
+var trans = { }
+
+trans.versions = ['bg', 'de', 'en', 'es', 'hu', 'pl'];
+
+trans.outofdatetranslations = ['bg', 'de', 'es', 'hu', 'pl'];
+
+trans.updatedtranslations = [];
+
+trans.unlinkedtranslations = [];
