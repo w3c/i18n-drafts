7c01aeef7b1e4cfb968217770618271837631397
r12a <ishida@w3.org>
2019-07-10 11:26:43 +0100

    questions/qa-headers-charset-data/translations, fr translation updated

--- questions/qa-headers-charset-data/translations.js
+++ questions/qa-headers-charset-data/translations.js
@@ -2,8 +2,8 @@ var trans = { }
 
 trans.versions = ['de','en','es','fr','hu','ko','pl','pt-br','ro','sv']
 
-trans.outofdatetranslations = ['fr','hu','pl']
+trans.outofdatetranslations = ['hu','pl']
 
-trans.updatedtranslations = []
+trans.updatedtranslations = ['fr']
 
 trans.unlinkedtranslations = ['ru','uk']
