6fcdc34966b45ba5ba80cd39cde9529a5d9e1d44
r12a <ishida@w3.org>
2021-06-23 16:13:59 +0100

    qa-html-language-declarations: Proposed rewrite updates format and links. Also changes an example per https://github.com/w3c/i18n-drafts/issues/237 Also removes .en from the default file.

--- questions/qa-html-language-declarations-data/translations.js
+++ questions/qa-html-language-declarations-data/translations.js
@@ -2,8 +2,8 @@ var trans = { }
 
 trans.versions = ['de','es', 'en','fr', 'it','pt-br','ru']
 
-trans.outofdatetranslations = []
+trans.outofdatetranslations = ['de','es', 'fr', 'it','pt-br','ru']
 
-trans.updatedtranslations = ['de','es']
+trans.updatedtranslations = []
 
 trans.unlinkedtranslations = ['uk']
