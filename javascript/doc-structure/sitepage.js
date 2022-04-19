4873e1764d9b5a30e17d6a1ee4b1ceb7e98cac2c
r12a <ishida@w3.org>
2022-04-19 16:49:12 +0100

    javascript/doc-structure/sitepage.js: Make translation links work when returning to English. English pages are now expected to NOT have .en.html, just .html.  Other language pages are unchanged.

--- javascript/doc-structure/sitepage.js
+++ javascript/doc-structure/sitepage.js
@@ -73,7 +73,9 @@ function stickyConneg (filename, cLang, targetLang) {
 		var path = ";path=/"
 		document.cookie = 'w3ci18nlang='+targetLang+expires+path
 		}
-	document.location.assign(filename+'.'+targetLang)
+	if (targetLang === 'en') targetLang = '.html'
+	else targetLang = '.'+targetLang+'.html'
+	document.location.assign(filename+targetLang)
 	}
 
 
