b72722e6129da9018c5ffeebf53a11a6504765f1
r12a <ishida@w3.org>
2022-05-16 18:08:20 +0100

    + characters.gl.html

--- getting-started/characters-data/translations.js
+++ getting-started/characters-data/translations.js
@@ -1,6 +1,6 @@
 var trans = { }
 
-trans.versions = ['ar', 'de', 'en', 'es', 'fr', 'hi', 'hu', 'pt', 'pt-br', 'ro', 'ru', 'sv', 'uk', 'zh-hans', 'zh-hant']
+trans.versions = ['ar', 'de', 'en', 'es', 'fr', 'gl', 'hi', 'hu', 'pt', 'pt-br', 'ro', 'ru', 'sv', 'uk', 'zh-hans', 'zh-hant']
 
 trans.outofdatetranslations = ['']
 
