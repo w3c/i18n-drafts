2133d6f3ff8f30d980bd2aa50ac58b0fe906160c
r12a <ishida@w3.org>
2022-05-19 11:12:32 +0100

    javascript/doc-structure/article-2022: In content neg links span -> bdi; add bullets

--- javascript/doc-structure/article-2022.js
+++ javascript/doc-structure/article-2022.js
@@ -93,8 +93,9 @@ if (trans.versions && !(trans.versions[0] == f.clang && trans.versions.length ==
 	versionList = '<p class="noprint">'
 	for (lang=0; lang<trans.versions.length; lang++) {
 		if (f.clang != trans.versions[lang]) {
-			versionList += '<span title="'+s.currLang[trans.versions[lang]]+'"><a href="#" onclick="stickyConneg(\''+f.filename+'\',\''+f.clang+'\',\''+trans.versions[lang]+'\'); return false;" lang="'+trans.versions[lang]+
-			'" translate="no" dir="auto">'+g.nativeText[trans.versions[lang]]+'</a></span>'+s.rlm+'&nbsp; ';
+			versionList += '<bdi title="'+s.currLang[trans.versions[lang]]+'"><a href="#" onclick="stickyConneg(\''+f.filename+'\',\''+f.clang+'\',\''+trans.versions[lang]+'\'); return false;" lang="'+trans.versions[lang]+
+			'" translate="no" dir="auto">'+g.nativeText[trans.versions[lang]]+'</a></bdi>'+s.rlm+'&#x202F;';
+			if (lang < trans.versions.length-1) versionList += '• &#x202F;'
 			}
 		}
 	versionList += '</p>';
