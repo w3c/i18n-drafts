36f3421680735e7977dff41acc0836362c68da5a
r12a <ishida@w3.org>
2022-06-17 14:24:42 +0100

    questions/qa-scripts-data/translations.js update

--- questions/qa-scripts-data/translations.js
+++ questions/qa-scripts-data/translations.js
@@ -1,9 +1,9 @@
 var trans = { }
 
-trans.versions = ['de','en','es','ru','uk'];
+trans.versions = ['de','en','es','ru','uk']
 
-trans.outofdatetranslations = [];
+trans.outofdatetranslations = ['de','es','ru','uk']
 
-trans.updatedtranslations = [];
+trans.updatedtranslations = []
 
-trans.unlinkedtranslations = [];
+trans.unlinkedtranslations = []
