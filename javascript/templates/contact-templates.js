6700b1cb667d3626e2fa0695d9424af533a49c68
Fuqiao Xue <xfq@w3.org>
2026-04-10 16:55:35 +0800

    tweak

--- javascript/templates/contact-templates.js
+++ javascript/templates/contact-templates.js
@@ -27,27 +27,6 @@ var contactTemplates = {
       '<li><a href="mailto:addisoni18n@gmail.com">Addison Phillips (addisoni18n @ gmail.com)</a>，国际化工作组主席</li>'+
       '<li><a href="mailto:duerst@it.aoyama.ac.jp">Martin Dürst (duerst @ it.aoyama.ac.jp)</a>，国际化兴趣组主席</li>'+
       '</ul>',
-
-    'participation-en': '<ul>'+
-      '<li><a href="mailto:xfq@w3.org">Fuqiao Xue (xfq @ w3.org)</a> Internationalization Lead; Internationalization Working Group Staff Contact</li>'+
-      '<li><a href="mailto:ishida@w3.org">Richard Ishida (ishida @ w3.org)</a>, Distinguished Expert, Internationalisation; Language Enablement Lead</li>'+
-      '<li><a href="mailto:addisoni18n@gmail.com">Addison Phillips (addisoni18n @ gmail.com)</a>, Internationalization Working Group Chair</li>'+
-      '<li><a href="mailto:duerst@it.aoyama.ac.jp">Martin Dürst (duerst @ it.aoyama.ac.jp)</a>, Internationalization Interest Group Chair</li>'+
-      '</ul>',
-
-    'participation-zh-hans': '<ul>'+
-      '<li><a href="mailto:xfq@w3.org">薛富侨 (xfq @ w3.org)</a>，国际化负责人，国际化工作组联系人</li>'+
-      '<li><a href="mailto:ishida@w3.org">Richard Ishida (ishida @ w3.org)</a>，国际化专家</li>'+
-      '<li><a href="mailto:addisoni18n@gmail.com">Addison Phillips (addisoni18n @ gmail.com)</a>，国际化工作组主席</li>'+
-      '<li><a href="mailto:duerst@it.aoyama.ac.jp">Martin Dürst (duerst @ it.aoyama.ac.jp)</a>，国际化兴趣组主席</li>'+
-      '</ul>',
-
-    'legacy-en': '<ul>'+
-      '<li><a href="mailto:xfq@w3.org">Fuqiao Xue (xfq @ w3.org)</a>, Internationalization Lead, Internationalization Working Group Staff Contact</li>'+
-      '<li><a href="mailto:ishida@w3.org">Richard Ishida (ishida @ w3.org)</a>, Internationalization Specialist</li>'+
-      '<li><a href="mailto:addisoni18n@gmail.com">Addison Phillips (addisoni18n @ gmail.com)</a>, Internationalization Working Group Chair</li>'+
-      '<li><a href="mailto:duerst@it.aoyama.ac.jp">Martin Dürst (duerst @ it.aoyama.ac.jp)</a>, Internationalization Interest Group Chair</li>'+
-      '</ul>'
   },
 
   render: function (targetId, variantName) {
