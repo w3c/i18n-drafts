5d342fb8b82b2661e7a5d2d1210e0f5458bd19e3
Fuqiao Xue <xfq@w3.org>
2026-07-24 09:17:47 +0800

    Use real translation URLs with progressive enhancement for the cookie prompt (#920)
    Fix #919.

--- javascript/doc-structure/article-2022.js
+++ javascript/doc-structure/article-2022.js
@@ -56,11 +56,25 @@ function showExample (path) {
 	}
 
 
-function stickyConneg (filename, cLang, targetLang) {
+function getLanguagePreferenceMessage (cLang, targetLang) {
+	var messages = {}
+	if (typeof cn !== 'undefined') messages = cn
+	var cMsg = messages[cLang]
+	var targetMsg = messages[targetLang]
+	if (cMsg && targetMsg) {
+		var msg = '['+cLang+'] '+cMsg
+		msg += '\n\n'+'['+targetLang+'] '+targetMsg
+		if (targetLang !== 'en' && cLang !== 'en' && messages.en) msg += '\n\n'+'[en] '+messages.en
+		return msg
+		}
+	return targetMsg || cMsg || s.cookieMsg || messages.en || ''
+	}
+
+function setLanguagePreference (targetLang, cLang) {
+	if (!targetLang) return
 	var response = false
-	var msg = '['+cLang+'] '+cn[cLang]
-	msg += '\n\n'+'['+targetLang+'] '+cn[targetLang]
-	if (targetLang !== 'en' && cLang !== 'en') msg += '\n\n'+'[en] '+cn.en
+	var msg = getLanguagePreferenceMessage(cLang || f.clang, targetLang)
+	if (!msg) return
 	response = confirm(msg)
 	if (response == true) {
 		var d = new Date()
@@ -69,7 +83,27 @@ function stickyConneg (filename, cLang, targetLang) {
 		var path = ";path=/"
 		document.cookie = 'w3ci18nlang='+targetLang+expires+path
 		}
-	document.location.assign(filename+'.'+targetLang+'.html')
+	}
+
+function stickyConneg (filename, cLang, targetLang) {
+	setLanguagePreference(targetLang, cLang)
+	document.location.assign(getTranslationUrl(filename, targetLang))
+	}
+
+function getTranslationUrl (filename, targetLang) {
+	if (f.translationPaths && f.translationPaths[targetLang]) return f.translationPaths[targetLang]
+	if (targetLang === 'en') return filename+'.en.html'
+	return filename+'.'+targetLang+'.html'
+	}
+
+function enhanceTranslationLinks () {
+	var links = document.querySelectorAll('a[data-lang]')
+	for (var l=0; l<links.length; l++) {
+		links[l].addEventListener('click', function (event) {
+			var link = event.currentTarget || this
+			setLanguagePreference(link.getAttribute('data-lang') || link.getAttribute('lang'))
+			})
+		}
 	}
 
 
@@ -99,7 +133,8 @@ if (trans.versions && !(trans.versions[0] == f.clang && trans.versions.length ==
 	versionList = '<p class="noprint">'
 	for (lang=0; lang<trans.versions.length; lang++) {
 		if (f.clang != trans.versions[lang]) {
-			versionList += '<bdi title="'+s.currLang[trans.versions[lang]]+'"><a href="#" onclick="stickyConneg(\''+f.filename+'\',\''+f.clang+'\',\''+trans.versions[lang]+'\'); return false;" lang="'+trans.versions[lang]+
+			var translationUrl = getTranslationUrl(f.filename, trans.versions[lang])
+			versionList += '<bdi title="'+s.currLang[trans.versions[lang]]+'"><a href="'+translationUrl+'" data-lang="'+trans.versions[lang]+'" lang="'+trans.versions[lang]+
 			'" translate="no" dir="auto">'+g.nativeText[trans.versions[lang]]+'</a></bdi>'+s.rlm+'&#x202F;';
 			if (lang < trans.versions.length-1) versionList += '• &#x202F;'
 			}
@@ -301,6 +336,7 @@ function completePage () {
 	//fillinTranslations()
 	if (document.getElementById("toclocation")) createtoc(true);
 	addSkipLink()
+	enhanceTranslationLinks()
 	getURLs()
 	}
 
